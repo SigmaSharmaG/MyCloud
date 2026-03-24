d2e12fb1dc5e5fcd4c8278757edcfb9d4823506b
Saksham Yadav <sakshamyadav260705@gmail.com>
2026-03-24 23:21:38 +0530

    landing page to home and new page security is created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,22 +7,26 @@ import Home from './pages/Home'
 import Vault from './pages/Vault'
 import Price from './pages/Price'
 import Contact from './pages/Contact'
+import Feature from './pages/Feature'
+import Security from './pages/Security'
 
 
 function App() {
 
   return (
     <>
+    
     <Routes>
+      
       <Route path='/login' element={<Login/>}/>
+      <Route path='/' element={<Home/>}/>
       <Route path='/signup' element={<Signup/>}/>
       <Route path='/Home' element={<Home/>}/>
-      <Route path='/Home' element={<Login/>}/>
+      <Route path='/Feature' element={<Feature/>}/>
       <Route path='/Vault' element={<Vault/>}/>
       <Route path='/Price' element={<Price/>}/>
       <Route path='/Contact' element={<Contact/>}/>
-
-
+      <Route path='/Security' element={<Security/>}/>
     </Routes>
     </>
   )
--- client/src/components/Navbar2.jsx
+++ client/src/components/Navbar2.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Navbar2 = () => {
+  const loction=useLocation();
+  const isSecurityPage=location.pathname==="/Security";
   return (
     <>
     {/* Navbar */}
-      <nav className="flex items-center justify-between px-16 py-4 ml-3 sticky top-0" >
+      <nav
+  className={`flex items-center justify-between px-16 py-4 ml-3 ${
+    isSecurityPage ? "relative" : "sticky top-0"
+  }`}
+>
+      
         {/* Logo */}
         <div className="text-xl font-bold flex items-center gap-2">
           <span className="bg-white text-blue-600 px-2 py-1 rounded">∑</span>
@@ -16,9 +24,9 @@ const Navbar2 = () => {
         <ul className="hidden md:flex gap-20 text-sm ">
           <Link to="/Home" ><li className="hover:text-gray-200 cursor-pointer">Home</li></Link>
           <Link to="/Feature"> <li className="hover:text-gray-200 cursor-pointer">Features</li></Link>
-          <li className="hover:text-gray-200 cursor-pointer">Pricing</li>
-          <li className="hover:text-gray-200 cursor-pointer">Security</li>
-          <li className="hover:text-gray-200 cursor-pointer">Contact</li>
+          <Link to="/Price"><li className="hover:text-gray-200 cursor-pointer">Pricing</li></Link>
+         <Link to="/Security"> <li className="hover:text-gray-200 cursor-pointer">Security</li></Link>
+          <Link to="/Contact"><li className="hover:text-gray-200 cursor-pointer">Contact</li></Link>
         </ul>
 
         {/* Menu Icon */}
